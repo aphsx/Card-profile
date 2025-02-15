96826aeb460f2f44b3aa8277965a92855dca1c60
aphsx <168500857+aphsx@users.noreply.github.com>
2025-02-15 10:37:05 +0700

    Add GitHub Pages support in Next.js configuration with basePath and assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: 'export', // ใช้ static export
+  basePath: isGithubActions ? '/my-new-profile' : '', // ตั้งค่า basePath สำหรับ GitHub Pages
+  assetPrefix: isGithubActions ? '/my-new-profile/' : '', // ตั้งค่า assetPrefix สำหรับ GitHub Pages
+  images: {
+    unoptimized: true, // ปิดการ optimize รูปภาพเพื่อให้ทำงานกับ static export
+  },
 };
 
 export default nextConfig;
