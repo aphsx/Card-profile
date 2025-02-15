cdf8fd9f7672608414248f3d1fede1d66697fd27
aphsx <168500857+aphsx@users.noreply.github.com>
2025-02-15 10:53:04 +0700

    Remove unused output files and update .gitignore to exclude the output directory

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
-const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
-
 const nextConfig: NextConfig = {
-  output: 'export', // ใช้ static export
-  basePath: isGithubActions ? '/my-new-profile' : '', // ตั้งค่า basePath สำหรับ GitHub Pages
-  assetPrefix: isGithubActions ? '/my-new-profile/' : '', // ตั้งค่า assetPrefix สำหรับ GitHub Pages
-  images: {
-    unoptimized: true, // ปิดการ optimize รูปภาพเพื่อให้ทำงานกับ static export
-  },
+  /* config options here */
 };
 
 export default nextConfig;
