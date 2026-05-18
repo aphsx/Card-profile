44afdc106a3a006dda6c67ad905cd8f7644643df
aphsx <aphisityns170960@gmail.com>
2026-05-18 21:41:43 +0700

    Add no-unused-vars, prefer-const, and no-console rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+      "prefer-const": "warn",
+      "no-console": ["warn", { "allow": ["warn", "error"] }],
+    },
+  },
 ];
 
 export default eslintConfig;
